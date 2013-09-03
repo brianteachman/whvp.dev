dadda5a478b0b1ed9bf82b33d343e14866ec411e
Brian Teachman <me@briant.me>
2013-09-03 13:27:16 -0700

    Commented out recaptcha settings in _config.php.

--- mysite/_config.php
+++ mysite/_config.php
@@ -75,10 +75,10 @@ HtmlEditorConfig::get('cms')->removeButtons('ssflash');
 GD::set_default_quality(85);
 
 // Dev server credentials, update to clients server for Live server
-RecaptchaField::$public_api_key = '6LfJVeUSAAAAAHpUka_PlERGrfwWYehZzj71Z9pR';
-RecaptchaField::$private_api_key = '6LfJVeUSAAAAADmR0c6w5A7qO2FvpTLNH5gEA3Qv';
-SpamProtectorManager::set_spam_protector('RecaptchaProtector');
-RecaptchaField::$private_api_key = '6LfJVeUSAAAAADmR0c6w5A7qO2FvpTLNH5gEA3Qv';
+// RecaptchaField::$public_api_key = '6LfJVeUSAAAAAHpUka_PlERGrfwWYehZzj71Z9pR';
+// RecaptchaField::$private_api_key = '6LfJVeUSAAAAADmR0c6w5A7qO2FvpTLNH5gEA3Qv';
+// SpamProtectorManager::set_spam_protector('RecaptchaProtector');
+// RecaptchaField::$private_api_key = '6LfJVeUSAAAAADmR0c6w5A7qO2FvpTLNH5gEA3Qv';
 
 
 // If DataObjects in the sitemap are needed, add extended dataobject
